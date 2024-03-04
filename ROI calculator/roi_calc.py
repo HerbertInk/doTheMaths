e2762ae1e2d4e5ccf3fbf660de2e6d08e23f1772
HerbertInk <herbertn200@gmail.com>
2024-03-04 03:11:35 +0300

    roi loop innit

--- ROI calculator/roi_calc.py
+++ ROI calculator/roi_calc.py
@@ -2,30 +2,42 @@
 # Takes in an instruments variables and
 # provides a ROI and Profit/Loss for the invested amount on execution
 
-# User input
-cryptocurrecy = input("Cryptocurrency: ")
-current_price = float(input("Current price: "))
-target_price = float(input("Target price: "))
-investment_amount = float(input("Investment amount: "))
+while True:
+    try:
+        # cryptocurrecy
+        cryptocurrecy = input("Cryptocurrency: ")
 
-# ROI calculator
-percentage_change = ((target_price - current_price) / current_price) * 100
-coins_tokens_available = investment_amount / current_price
+        # Check if cryptocurrency input is not empty 'contains at least one character'
+        if not cryptocurrecy: # or not any(char.isalpha() for char in cryptocurrecy):
+            print("Cryptocurrency is required")
+            continue  # Allow user retry
 
-unrealized_bal = target_price * coins_tokens_available
-roi = ((unrealized_bal - investment_amount) / investment_amount) * 100
+        current_price = float(input("Current price: "))
+        target_price = float(input("Target price: "))
+        investment_amount = float(input("Investment amount: "))
 
-profit = (roi * investment_amount) / 100
+        # ROI calculator
+        percentage_change = ((target_price - current_price) / current_price) * 100
+        coins_tokens_available = investment_amount / current_price
 
-# Prints
-print("\n-----CURRENCY--COMMODITY--BAL--ROI--P/L-----")
-print(f"Cryptocurrecy: {cryptocurrecy}")
-print(f"Price change in %: O{current_price} C{target_price} ({percentage_change:.2f}%)")
-print(f"{cryptocurrecy} balance: {coins_tokens_available:.8f}")
-print(f"Investment Amount: ${investment_amount:.2f}")
-print("----------------------------------------------")
-print(f"Unrealized Balance: {unrealized_bal:.2f}")
-print(f"ROI: {roi:.2f}%")
-print(f"P/L: ${profit:.2f}")
-print("----------------------------------------------")
-print("--Risk disclosure: Not financial advice--")
+        unrealized_bal = target_price * coins_tokens_available
+        roi = ((unrealized_bal - investment_amount) / investment_amount) * 100
+
+        profit = (roi * investment_amount) / 100
+
+        # Prints
+        print("\n-----CURRENCY--COMMODITY--BAL--ROI--P/L-----")
+        print(f"Cryptocurrecy: {cryptocurrecy}")
+        print(f"Price change in %: Open {current_price} Close {target_price} ({percentage_change:.2f}%)")
+        print(f"{cryptocurrecy} balance: {coins_tokens_available:.8f}")
+        print(f"Investment Amount: ${investment_amount:.2f}")
+        print("----------------------------------------------")
+        print(f"Unrealized Balance: {unrealized_bal:.2f}")
+        print(f"ROI: {roi:.2f}%")
+        print(f"P/L: ${profit:.2f}")
+        print("----------------------------------------------")
+        print("--Risk disclosure: Not financial advice--")
+        break # Exit on successful execution
+    except:
+        print("Please enter valid numeric values")
+        continue # Allow user retry
